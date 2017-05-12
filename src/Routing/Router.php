e63fcab7c0fab94800ad6ec585ac4ed1914f6da4
Saša Stamenković <umpirsky@gmail.com>
2017-05-12 14:36:57 +0200

    Override Symfony\Bundle\FrameworkBundle\Routing\Router

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -2,7 +2,7 @@
 
 namespace Umpirsky\I18nRoutingBundle\Routing;
 
-use Symfony\Component\Routing\Router as SymfonyRouter;
+use Symfony\Bundle\FrameworkBundle\Routing\Router as SymfonyRouter;
 
 class Router extends SymfonyRouter
 {
