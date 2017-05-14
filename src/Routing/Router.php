a60de27cb83581614bf3e6272ab067d52f7f407d
Saša Stamenković <umpirsky@gmail.com>
2017-05-14 12:00:44 +0200

    Increase code readability

--- src/Routing/Router.php
+++ src/Routing/Router.php
@@ -9,17 +9,24 @@ class Router extends BaseRouter
 {
     public function generate($name, $parameters = [], $referenceType = self::ABSOLUTE_PATH)
     {
-        if (array_key_exists('_locale', $parameters) || $this->context->hasParameter('_locale')) {
-            $i18nName = $name.'_i18n';
-
-            $i18nParameters = $parameters;
-            $i18nParameters['_locale'] = array_key_exists('_locale', $parameters) ? $parameters['_locale'] : $this->context->getParameter('_locale');
-
-            try {
-                return parent::generate($i18nName, $i18nParameters, $referenceType);
-            } catch (RouteNotFoundException $e) { }
-        }
+        try {
+            return $this->generateI18n($name, $parameters, $referenceType);
+        } catch (RouteNotFoundException $e) { }
 
         return parent::generate($name, $parameters, $referenceType);
+    }
+
+    /**
+     * @todo Inject route name suffix
+     */
+    private function generateI18n($name, $parameters = [], $referenceType = self::ABSOLUTE_PATH)
+    {
+        if (!array_key_exists('_locale', $parameters) && !$this->context->hasParameter('_locale')) {
+            return parent::generate($name, $parameters, $referenceType);
+        }
+
+        $parameters['_locale'] = array_key_exists('_locale', $parameters) ? $parameters['_locale'] : $this->context->getParameter('_locale');
+
+        return parent::generate($name.'_i18n', $parameters, $referenceType);
     }
 }
