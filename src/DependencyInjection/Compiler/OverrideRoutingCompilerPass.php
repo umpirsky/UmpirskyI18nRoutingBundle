7c8d92c1e7b184252999c4bfc5fb1999234cb3f9
Kevin Bond <kevinbond@gmail.com>
2017-11-10 08:11:31 -0500

    use service decoration for i18n_route_loader

--- src/DependencyInjection/Compiler/OverrideRoutingCompilerPass.php
+++ src/DependencyInjection/Compiler/OverrideRoutingCompilerPass.php
@@ -4,26 +4,21 @@ namespace Umpirsky\I18nRoutingBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Alias;
-use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
 use Umpirsky\I18nRoutingBundle\Routing\Router;
 
 class OverrideRoutingCompilerPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container)
     {
-        if ('prefix_except_default' === $container->getParameter('umpirsky_i18n_routing.strategy')) {
-            $routerDefinition = $container->getDefinition('router.default');
-            $routerDefinition->setClass(Router::class);
-            $options = $routerDefinition->getArgument(2);
-            $options['i18n_route_name_suffix'] = $container->getParameter('umpirsky_i18n_routing.route_name_suffix');
-            $options['i18n_default_locale'] = $container->getParameter('umpirsky_i18n_routing.default_locale');
-            $routerDefinition->replaceArgument(2, $options);
+        if ('prefix_except_default' !== $container->getParameter('umpirsky_i18n_routing.strategy')) {
+            return;
         }
 
-        $routingLoaderDefinition = $container->getDefinition('routing.loader');
-        $routingLoaderDefinition->setPublic(false);
-        $container->setDefinition('umpirsky_i18n_routing.routing.loader.i18n_route_loader.parent', $routingLoaderDefinition);
-        $container->setAlias('routing.loader', 'umpirsky_i18n_routing.routing.loader.i18n_route_loader');
+        $routerDefinition = $container->getDefinition('router.default');
+        $routerDefinition->setClass(Router::class);
+        $options = $routerDefinition->getArgument(2);
+        $options['i18n_route_name_suffix'] = $container->getParameter('umpirsky_i18n_routing.route_name_suffix');
+        $options['i18n_default_locale'] = $container->getParameter('umpirsky_i18n_routing.default_locale');
+        $routerDefinition->replaceArgument(2, $options);
     }
 }
