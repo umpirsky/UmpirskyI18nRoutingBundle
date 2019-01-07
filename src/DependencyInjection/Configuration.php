1ad50eba5beebc5672e83fbc9916de10e9516e6b
Kevin Bond <kevinbond@gmail.com>
2019-01-07 09:28:20 -0500

    fix TreeBuilder deprecation

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -9,23 +9,28 @@ final class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
+        $treeBuilder = new TreeBuilder('umpirsky_i18n_routing');
 
-        $treeBuilder
-            ->root('umpirsky_i18n_routing')
-                ->children()
-                    ->scalarNode('route_name_suffix')->defaultValue('_i18n')->end()
-                    ->scalarNode('default_locale')->isRequired()->end()
-                    ->arrayNode('locales')
-                        ->requiresAtLeastOneElement()
-                        ->prototype('scalar')->end()
-                    ->end()
-                    ->scalarNode('strategy')
-                        ->defaultValue('prefix')
-                        ->validate()
-                            ->ifNotInArray(array('prefix', 'prefix_except_default'))
-                            ->thenInvalid('Must be prefix (default) or prefix_except_default')
-                        ->end()
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('umpirsky_i18n_routing');
+        }
+
+        $rootNode
+            ->children()
+                ->scalarNode('route_name_suffix')->defaultValue('_i18n')->end()
+                ->scalarNode('default_locale')->isRequired()->end()
+                ->arrayNode('locales')
+                    ->requiresAtLeastOneElement()
+                    ->prototype('scalar')->end()
+                ->end()
+                ->scalarNode('strategy')
+                    ->defaultValue('prefix')
+                    ->validate()
+                        ->ifNotInArray(array('prefix', 'prefix_except_default'))
+                        ->thenInvalid('Must be prefix (default) or prefix_except_default')
                     ->end()
                 ->end()
             ->end()
