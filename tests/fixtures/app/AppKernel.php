eef824bdf3a968f9a87efafebdaeb47f3e633cc1
Kevin Bond <kevinbond@gmail.com>
2021-02-18 09:42:40 -0500

    dependency upgrade:
    - allow php 8
    - allow symfony 5+
    - bump min Symfony version to 4.4+

--- tests/fixtures/app/AppKernel.php
+++ tests/fixtures/app/AppKernel.php
@@ -15,6 +15,6 @@ class AppKernel extends Kernel
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
     }
 }
